836aff03269e544af95c1a828cbdf4ebb3e0956c
Halle Starns <halle@Halles-MacBook-Pro.local>
2019-09-13 23:36:00 -0500

    Update kaprekarUtils.c
    Added semicolo and declared variable modulous

--- c_version/kaprekarUtils.c
+++ c_version/kaprekarUtils.c
@@ -37,7 +37,8 @@ int isKaprekar(int n) {
   int numDigits = (int) log10(n) + 1;
   long modulus = 0;
   long first, second;
-  int numberOfDigits  
+  int numberOfDigits;
+  int modulous;  
   //for each possible "split" of the square...
   for(i=1; i<=numberOfDigits; i++) {
     //increase the modulus by a factor of 10
